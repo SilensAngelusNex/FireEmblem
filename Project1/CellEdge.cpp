1f8f8115e7dc8769f69c1cbd6aafd4240f2072e6
Torrey Kelly <tkelly14@gmail.com>
2018-06-06 20:25:24 -0400

    CellEdge temp

--- Project1/CellEdge.cpp
+++ Project1/CellEdge.cpp
@@ -1,9 +1,9 @@
 #include "CellEdge.h"
-
+#include "GridCell.h"
 
 
 CellEdge::CellEdge(GridCell* cell, MobilityList<std::optional<int>> costs) :
-	_cell(cell), 
+	_cell(cell),
 	_costs(costs)
 {}
 
@@ -16,6 +16,30 @@ bool CellEdge::canTraverse(MobilityType mobility)
 {
 	return bool(_costs[mobility].has_value());
 }
+
+std::optional<int> CellEdge::getCost(MobilityList<bool> mobility_type, bool intangible) {
+
+	std::optional<int> cost;
+	/*
+	for (MobilityType mobility : MobilityType::list) {
+	if (mobility_type[mobility]) {
+	std::optional<int> edge_cost = getCost(mobility);
+	if (edge_cost.has_value() && canPass(intangible)) { // if we can step on the tile
+	if (cost < edge_cost) { // if the cost is best found yet
+	cost = edge_cost.value();
+	}
+	}
+	}
+	}
+	*/
+	return cost;
+}
+
+bool CellEdge::canPass(bool intangible) {
+	return false;
+	//return intangible || !(_cell->getTile().hasUnit());
+}
+
 
 /**
 This function allows me to use list.remove() on CellEdge. Probably a bad idea
